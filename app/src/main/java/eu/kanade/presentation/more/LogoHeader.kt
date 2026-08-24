4bccedeb647d487807a31a4cb9f2bffd8d07749e
hongducdev <contact.hongduc@gmail.com>
2026-08-24 08:42:39 +0700

    feat(branding): refresh app artwork

--- app/src/main/java/eu/kanade/presentation/more/LogoHeader.kt
+++ app/src/main/java/eu/kanade/presentation/more/LogoHeader.kt
@@ -6,10 +6,12 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import eu.kanade.tachiyomi.R
@@ -27,7 +29,8 @@ fun LogoHeader(
             contentDescription = null,
             modifier = Modifier
                 .padding(iconPadding)
-                .size(64.dp),
+                .size(64.dp)
+                .clip(RoundedCornerShape(12.dp)),
         )
 
         HorizontalDivider()
